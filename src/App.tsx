844bd4bf2389088635521d31c88a773148bbd845
GorelovaP <komsomolskai1@mail.ru>
2022-08-12 18:52:17 +0300

    Correct 35

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,29 @@ import {Header} from "./Components/Header/Header";
 import {Nav} from "./Components/Nav/Nav";
 import {Pages} from "./Components/Pages/Pages";
 import {Dialogs} from "./Components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import {Settings} from "./Components/Settings/Settings";
 import {Music} from "./Components/Music/Music";
 import {stateTypeRootPage} from "./Redux/state";
 
 function App(props: stateTypeRootPage) {
-    debugger
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Nav/>
-                <div className='app-wrapper-content'>
-                    <Route path={"/dialogs"} element={ <Dialogs dialogsPage={props.state.dialogsPage}/>}/>
+
+        <div className='app-wrapper'>
+            <Header/>
+            <Nav/>
+            <div className='app-wrapper-content'>
+                <Routes>
+                    <Route path={"/dialogs"} element={<Dialogs dialogsPage={props.state.dialogsPage}/>}/>
                     <Route path={"/pages"}
                            element={<Pages profilePage={props.state.profilePage} addPost={props.addPost}
                                            ChangeNewPostText={props.ChangeNewPostText}/>}/>
                     <Route path={"/music"} element={<Music/>}/>
                     <Route path={"/settings"} element={<Settings/>}/>
-                </div>
+                </Routes>
             </div>
-        </BrowserRouter>
+        </div>
+
 
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
 import './index.css';
-
-import {subscribe} from "./Redux/state";
-import {renderTree} from "./render";
+import React from "react";
+import {AddPosts, ChangeNewPostText, stateTypeRoot, subscribe} from "./Redux/state";
 import reportWebVitals from './reportWebVitals';
+import {state} from "./Redux/state";
+import App from "./App";
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 
-debugger
-subscribe(renderTree);
+
+// After
+
+
+const renderTree = (state: stateTypeRoot) => {
+
+    ReactDOM.render(<BrowserRouter>
+        <App state={state} addPost={AddPosts}
+             ChangeNewPostText={ChangeNewPostText}/>
+    </BrowserRouter>, document.getElementById('root'));
+
+}
+renderTree(state)
+subscribe(() => renderTree(state));
 reportWebVitals()
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWAserviceWorker.unregister();
