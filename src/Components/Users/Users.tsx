3f656b226e5bcb0ed9d3a184755348a0220463f7
GorelovaP <komsomolskai1@mail.ru>
2022-08-28 16:27:26 +0300

    53 - Class component (extends React.Component)

--- src/Components/Users/Users.tsx
+++ src/Components/Users/Users.tsx
@@ -1,35 +1,37 @@
 import React from "react";
+import s from "./Users.module.css";
+import standardIcon from "../assets/images/standardIcon.png";
 import {UsersPagePropsType} from "./UsersContainer";
-import s from './Users.module.css';
 import axios from "axios";
-import standardIcon from "../assets/images/standardIcon.png"
+
+export class Users extends React.Component<UsersPagePropsType> {
+
+    constructor(props: UsersPagePropsType) {
+        super(props);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(response => {
+            this.props.setUsers(response.data.items)
+        })
 
 
-export const Users = (props: UsersPagePropsType) => {
-    let GetUsers = () => {
-        if (props.users.length === 0) {
-            debugger
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(response => {
-                props.setUsers(response.data.items)
-            })
-            debugger
-        }
     }
-    let Follow = (userId: number) => {
-        props.follow(userId)
+
+
+    Follow = (userId: number) => {
+        this.props.follow(userId)
     }
-    let Unfollow = (userId: number) => {
-        props.unfollow(userId)
+    Unfollow = (userId: number) => {
+        this.props.unfollow(userId)
 
     }
-    return (<div className={s.usersContainer}>
+
+    render() {
+        return <div className={s.usersContainer}>
             <h2 className={s.usersContainer__logo}>Users</h2>
             <div className={s.actionBlock}> поиск, по региону и т д</div>
-            <button onClick={GetUsers}>Get users</button>
             <div className={s.usersBlock}>
                 {
-                    props.users.map(
-                        (u) =>
+                    this.props.users.map(
+                        u =>
                             <div className={s.usersBlock__user} key={u.id}>
                                 <div className={s.usersBlock__user__avatar}>
                                     <img alt="ava" src={u.photos.small != null ? u.photos.small : standardIcon}
@@ -40,8 +42,8 @@ export const Users = (props: UsersPagePropsType) => {
                                 <div>{u.status}</div>
 
                                 <div>
-                                    {u.followed ? <button onClick={() => Unfollow(u.id)}> Unfollow</button> :
-                                        <button onClick={() => Follow(u.id)}>Follow</button>
+                                    {u.followed ? <button onClick={() => this.Unfollow(u.id)}> Unfollow</button> :
+                                        <button onClick={() => this.Follow(u.id)}>Follow</button>
                                     }
                                 </div>
                             </div>
@@ -50,5 +52,5 @@ export const Users = (props: UsersPagePropsType) => {
             </div>
 
         </div>
-    )
+    }
 }
