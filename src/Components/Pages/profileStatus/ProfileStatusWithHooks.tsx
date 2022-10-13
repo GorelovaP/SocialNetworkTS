7a2a75449855339fe8651e831e57c9149321c504
GorelovaP <komsomolskai1@mail.ru>
2022-10-13 21:35:00 +0300

    84 - hook, useState

---
+++ src/Components/Pages/profileStatus/ProfileStatusWithHooks.tsx
@@ -0,0 +1,39 @@
+import React, {ChangeEvent, useState} from "react";
+
+type ProfileStatusPropsType = {
+    status: string;
+    updateStatus: (status: string) => void
+}
+
+export const ProfileStatusWithHooks = (props: ProfileStatusPropsType) => {
+    let [editMode, setEditMode] = useState<boolean>(false)
+    let [status, setStatus] = useState<string>(props.status)
+
+    return (
+        <div>
+            {!editMode &&
+            <div>
+                        <span onDoubleClick={() => {
+                            setEditMode(true)
+                        }}>
+                            {status || "----"}
+                        </span>
+            </div>
+            }
+            {editMode &&
+            <div>
+                <input
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setStatus(e.currentTarget.value)}
+                    autoFocus={true}
+                    onBlur={() => {
+                        setEditMode(false)
+                        props.updateStatus(status)
+                    }}
+                    value={status}
+                    type="text"/>
+            </div>
+            }
+        </div>
+    )
+}
+
